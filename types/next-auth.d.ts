4105635ac653e289e914993ce504000fc0595d39
hiraaziz <hiraaziz1998@gmail.com>
2023-07-08 02:33:25 +0500

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      email: string;
+      accesstoken: string;
+      iat: number;
+      exp: number;
+      jti: string;
+    };
+  }
+}
